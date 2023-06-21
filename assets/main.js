3ac7a76dce28c467f9eb9c1f1ed7ec35f9b88d68
thaethae-nikkoo <thaenandarsoe.2201@gmail.com>
2023-06-21 21:27:53 +0630

    initial commit my personal website

---
+++ assets/main.js
@@ -0,0 +1,37 @@
+// $(document).ready(function () {
+//   $(".slick").slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     centerMode: true,
+//     centerPadding: "60px",
+//     slidesToShow: 3,
+//     autoplay: true,
+//     autoplaySpeed: 1500,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//           infinite: true,
+//           // dots: true,
+//         },
+//       },
+//       {
+//         breakpoint: 600,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//     ],
+//   });
+// });
